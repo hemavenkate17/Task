5612e99d1c01f98bad8a08a5b8ab8930d6096b9c
hemavenkate17 <82799233+hemavenkate17@users.noreply.github.com>
2021-04-29 19:13:29 +0530

    Update task query.sql

--- task query.sql
+++ task query.sql
@@ -3,19 +3,20 @@ select city , count(*) au_id from authors group by city
 select au_fname,au_lname  from authors where city not in 
 (select distinct pub_name ='New Moon Books'from publishers)
 
-create proc proc_author(@eau_fname varchar(50), @eau_lname varchar(50),@eprice float)
+create proc proc_author(@eau_fname varchar(50), @eau_lname varchar(50),@enewprice float)
 as
 begin
   declare
-  @enewprice float = 100
+  @eprice float
   
     set @eprice = @enewprice 
-	select @eprice as NewPrice from titles 
+    select @eprice as NewPrice from titles 
     select @eau_fname as FirstName,@eau_lname as LastName from authors
-	where authors.au_fname=@eau_fname and authors.au_lname=@eau_lname
+    where authors.au_fname=@eau_fname and authors.au_lname=@eau_lname
 end
 
-exec proc_author 'johnson','white',19.99
+exec proc_author 'johnson','white',100
+
 
 drop proc proc_author
 
