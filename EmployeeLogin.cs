571a8d73266e4f1a5392c182f3dd7fbddc03d28b
hemavenkate17 <82799233+hemavenkate17@users.noreply.github.com>
2021-05-06 20:08:33 +0530

    Add files via upload

---
+++ EmployeeLogin.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransportBLLibrary;
+using TransportDALLibrary;
+
+
+namespace TransportFEApplication
+{
+    class EmployeeLogin
+    {
+        private IUserLogin<Employee> _login;
+        public EmployeeLogin()
+        {
+
+        }
+        public EmployeeLogin(IUserLogin<Employee> login)
+        {
+            _login = login;
+        }
+       public void UserLogin()
+        {
+            Console.WriteLine("Please enter the employee id");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the password");
+            string password = Console.ReadLine();
+            Employee employee = new Employee();
+            employee.Id = id;
+            employee.Password = password;
+            try
+            {
+                if (_login.Login(employee))
+                    Console.WriteLine("welcome");
+                else
+                    Console.WriteLine("incorrect id or password");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Login exception");
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void RegisterEmployee()
+        {
+            CompleteEmployee employee = new CompleteEmployee();
+            employee.TakeEmployeeData();
+            try
+            {
+                if (_login.Add(employee))
+                    Console.WriteLine("Employee registered");
+                else
+                    Console.WriteLine("Sorry could not complete creating an employee");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("could not add employee");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,221 +1,67 @@
 ﻿using System;
-using System.Data;
 using System.Data.SqlClient;
+using System.Data;
+using System.Collections.Generic;
+using TransportBLLibrary;
 
 
-namespace AdoExampleProject
+namespace TransportFEApplication
 {
     class Program
     {
-        string conString;
-        SqlConnection con;
-        SqlCommand cmd;
-
-     
-
-    public Program()
+        EmployeeLogin login;
+        EmployeeManagement management;
+        EmployeeBL bl;
+        public Program()
         {
-            conString = @"server=LAPTOP-FUHQ3D30\SQLEXPRESS;Integrated security=true;Initial catalog=pubs";
-            con = new SqlConnection(conString);
+            bl = new EmployeeBL();
+            login = new EmployeeLogin(bl);
+            management = new EmployeeManagement(bl);
         }
-
         void PrintMenu()
         {
             int choice = 0;
             do
             {
-                Console.WriteLine("Menu");
-                Console.WriteLine("1. Print all the movies");
-                Console.WriteLine("2. fetch one movie from Database");
-                Console.WriteLine("3. Add movie details");
-                Console.WriteLine("4. update movie duration");
-                Console.WriteLine("5. Delete movie details");
+                Console.WriteLine("1. Register");
+                Console.WriteLine("2. Login");
+                Console.WriteLine("3. Print all");
+                Console.WriteLine("4. Sort and print all");
+                Console.WriteLine("5. Update Password");
+                Console.WriteLine("6. Exit");
                 choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
                     case 1:
-                        PrintMovie();
+                        login.RegisterEmployee();
                         break;
                     case 2:
-                        FetchOneMoviesFromDatabase();
+                        login.UserLogin();
                         break;
                     case 3:
-                        AddMovie();
+                        management.PrintAllEmployee();
                         break;
                     case 4:
-                        UpdateMovieDuration();
+                        management.PrintEmployeeSortedById();
                         break;
                     case 5:
-                        DeleteMovie();
+                        management.ResetPassword();
+                        break;
+                    case 6:
+                        Console.WriteLine("exited ");
                         break;
                     default:
                         Console.WriteLine("Invalid choice");
                         break;
                 }
-            } while (choice != 6);
+
+            } while (choice !=7);
         }
-       void PrintMovie()
+        static void Main(string[] args)
         {
-            string strcmd = "select* from tblMovie";
-            cmd = new SqlCommand(strcmd, con);
-            try
-            {
-                con.Open();
-                SqlDataReader drMovies = cmd.ExecuteReader();
-                while (drMovies.Read())
-                {
-                    Console.WriteLine("Movie Id " + drMovies[0].ToString());
-                    Console.WriteLine("Movie Name " + drMovies[1]);
-                    Console.WriteLine("Movie Duration " + drMovies[2].ToString());
-                    Console.WriteLine("----------------");
-
-                }
-            }
-            catch (SqlException sqlException)
-            {
-                Console.WriteLine(sqlException.Message);
-            }
-            finally
-            {
-                con.Close();
-            }
+            Console.WriteLine("Hello World!");
+            new Program().PrintMenu();
+            Console.ReadKey();
         }
-
-        void FetchOneMoviesFromDatabase()
-        {
-            string strcmd = "select* from tblMovie where id = @mid";
-            cmd = new SqlCommand(strcmd, con);
-            try
-            {
-                con.Open();
-                Console.WriteLine("Please enter the id");
-                int id = Convert.ToInt32(Console.ReadLine());
-                cmd.Parameters.Add("@mid",SqlDbType.Int);
-                cmd.Parameters[0].Value = id;
-                SqlDataReader drMovies = cmd.ExecuteReader();
-                while (drMovies.Read())
-                {
-                    Console.WriteLine("Movie Id" + " "+ drMovies[0].ToString());
-                    Console.WriteLine("Movie Name" + " " + drMovies[1]);
-                    Console.WriteLine("Movie Duration" + " " + drMovies[2].ToString());
-                    Console.WriteLine("----------------");
-
-                }
-            }
-            catch (SqlException sqlException)
-            {
-                Console.WriteLine(sqlException.Message);
-            }
-            finally
-            {
-                con.Close();
-            }
-        }
-
-        void AddMovie()
-        {
-            Console.WriteLine("enter the movie name");
-            string mName = Console.ReadLine();
-            Console.WriteLine("enter movie duration");
-            float mDuration = (float)Math.Round(float.Parse(Console.ReadLine()), 2);
-            string strCmd = "insert into tblMovie(name,duration) values(@mname,@mdur)";
-            cmd = new SqlCommand(strCmd, con);
-            cmd.Parameters.AddWithValue("@mname", mName);
-            cmd.Parameters.AddWithValue("@mdur", mDuration);
-            try
-            {
-                con.Open();
-                int result = cmd.ExecuteNonQuery();
-                if (result > 0)
-                    Console.WriteLine("Movie inserted into the Database");
-                else
-                    Console.WriteLine("No not done");
-
-            }
-            catch (Exception e)
-            {
-
-                Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                con.Close();
-            }
-        }
-        void UpdateMovieDuration()
-        {
-            
-            Console.WriteLine("Please enter the movie Id");
-            int id = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter the movie duration");
-            float mDuration = (float)Math.Round(float.Parse(Console.ReadLine()), 2);
-            string strCmd = "Update tblMovie set duration = @mduration where id = @mid";
-            cmd = new SqlCommand(strCmd, con);
-            cmd.Parameters.AddWithValue("@mid", id);
-            cmd.Parameters.AddWithValue("@mduration", mDuration);
-            try
-            {
-                con.Open();
-                int result = cmd.ExecuteNonQuery();
-                if (result > 0)
-                    Console.WriteLine("Movie details Updated");
-                else
-                    Console.WriteLine("No not done");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                con.Close();
-            }
-        }
-
-       
-        void DeleteMovie()
-        {
-            Console.WriteLine("Please enter the movie name");
-            string mName= Console.ReadLine();
-            Console.WriteLine("Please enter the movie Duration");
-            string mDuration = Console.ReadLine();
-            string strCmd = "Delete from tblMovie where name=@mName";
-            cmd = new SqlCommand(strCmd, con);
-            cmd.Parameters.AddWithValue("@mname", mName);
-            cmd.Parameters.AddWithValue("@mduration", mDuration);
-            try
-            {
-                con.Open();
-                int result = cmd.ExecuteNonQuery();
-                if (result > 0)
-                    Console.WriteLine("Movie Details deleted from the Database");
-                else
-                    Console.WriteLine("no not done");
-
-            }
-            catch (Exception e)
-            {
-
-                Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                con.Close();
-            }
-
-        }
-            static void Main(string[] args)
-            {
-                
-                Program program = new Program();
-                program.PrintMenu();
-                program.AddMovie();
-                program.FetchOneMoviesFromDatabase();
-                program.UpdateMovieDuration();
-                program.DeleteMovie();
-                Console.ReadKey();
-            }
-
-        
     }
 }
